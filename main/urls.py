21edc30c962fa837fd9dbd989a21e68320a034ba
WietsBehr (31770509) <70149733+WietsBehr@users.noreply.github.com>
2021-11-04 15:55:17 +0200

    Added CSS and Bootstrap customization.

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index" ),
+    path("index/", views.index, name="index"),
+    path('', views.login, name="login"),
+    path('test/', views.test, name="test"),
+    path('register/', views.register, name="register"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(response):
-    return HttpResponse("<h1>My project<h1>")
+    return render(response, "main/base.html", {})
+
+def login(response):
+    return render(response, "main/login.html", {})
+
+def register(response):
+    return render(response, "main/register.html", {})
+
+def test(response):
+    return render(response, "main/test.html", {})
+
