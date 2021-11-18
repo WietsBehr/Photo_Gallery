b6179cbd4c5e73d2ebafa470592b3c576b5c3115
WietsBehr (31770509) <70149733+WietsBehr@users.noreply.github.com>
2021-11-18 17:58:25 +0200

    Initial rebase

--- CMPG_323_Project_2/urls.py
+++ CMPG_323_Project_2/urls.py
@@ -1,4 +1,4 @@
-"""Photo_Gallery URL Configuration
+"""CMPG_323_Project_2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from users import views as user_views
+from main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls")),
+    path('register/', user_views.register, name='register'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('gallery/', main_views.gallery, name='gallery'),
+    path('upload/', main_views.upload, name='upload'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
